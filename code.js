4964f1ccd14bf37905a97501a4e99367b249aa8c
Evan Kallas <144737982+egkallas@users.noreply.github.com>
2024-02-05 16:07:31 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,16 +1,15 @@
 function fib(n) {
 
     if(n === 0){
-        return 0;
+        return [0];
     }
     else if(n === 1){
-        return 1;
+        return [0, 1];
     }
     else{
-        return fib(n-1) + fib(n-2);
+        fibArray = fib(n - 1);
+        fibArray.push(fibArray[fibArray.length - 1] + fibArray[fibArray.length - 2]);
+        return fibArray;
     }
 
 }
-
-/*I realize this is supposed to return an array of the fib numbers, but I'm not sure how to do this recursively.
-Can you give me any suggestions?*/
